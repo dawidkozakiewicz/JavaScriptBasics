2a08eb90d1c06c411d3184e8034a41c89f1a5ac0
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-08 11:12:05 +0200

    file todo.js modified

--- JavaScript/arrays/todo.js
+++ JavaScript/arrays/todo.js
@@ -1,8 +1,10 @@
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
 
 todos.splice(2, 1)
+console.log(todos)
 todos.push('Buy coffee')
+console.log(todos)
 todos.shift()
-
+console.log(todos)
 console.log(`You have ${todos.length} todos!`)
 console.log(todos)
