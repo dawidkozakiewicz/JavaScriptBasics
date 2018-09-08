659f048b37918bc63b7a12e2078649c99f252e08
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-08 14:57:06 +0200

    mannipulation

--- JavaScript/arrays/todo.js
+++ JavaScript/arrays/todo.js
@@ -1,10 +1,8 @@
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
 
 todos.splice(2, 1)
-console.log(todos)
 todos.push('Buy coffee')
-console.log(todos)
 todos.shift()
-console.log(todos)
+
 console.log(`You have ${todos.length} todos!`)
 console.log(todos)
