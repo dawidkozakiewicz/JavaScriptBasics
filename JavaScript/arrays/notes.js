2be904005df2b18db018024327f47a9d4b848ead
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-08 18:57:44 +0200

    searchII

--- JavaScript/arrays/notes.js
+++ JavaScript/arrays/notes.js
@@ -9,25 +9,18 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-// console.log(notes.pop())
-// notes.push('My new note')
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
 
-// console.log(notes.shift())
-// notes.unshift('My first note')
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
 
-// notes.splice(1, 1, 'This is the new second item')
-
-// notes[2] = 'This is now the new note 3'
-
-// notes.forEach(function (item, index) {
-//     console.log(index)
-//     console.log(item)
-// })
-
-console.log(notes.length)
-console.log(notes)
-
-const index = notes.findIndex(function (note, index) {
-    return note.title === 'Habbits to work on'
-})
-console.log(index)
+const note = findNote(notes, 'some other office modification')
+console.log(note)
