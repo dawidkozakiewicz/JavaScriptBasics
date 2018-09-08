147276d723245d18711d364ad39bc848dac2889d
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-08 18:02:23 +0200

    search

--- JavaScript/arrays/notes.js
+++ JavaScript/arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -8,12 +17,17 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (item, index) {
-    console.log(index)
-    console.log(item)
-})
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
 console.log(notes.length)
 console.log(notes)
+
+const index = notes.findIndex(function (note, index) {
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
