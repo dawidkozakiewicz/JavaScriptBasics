49afa7059eece41db2e7ce793cdad8f4f0c16830
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-09 16:29:27 +0200

    sorting

--- JavaScript/arrays/todo.js
+++ JavaScript/arrays/todo.js
@@ -15,6 +15,18 @@ const todos = [{
     completed: true
 }]
 
+const sortTodos = function (todos) {
+    todos.sort(function (a, b) {
+        if (!a.completed && b.completed) {
+            return -1
+        } else if (!b.completed && a.completed) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
 const deleteTodo = function (todos, todoText) {
     const index = todos.findIndex(function (todo) {
         return todo.text.toLowerCase() === todoText.toLowerCase()
@@ -31,7 +43,10 @@ const getThingsToDo = function (todos) {
     })
 }
 
-console.log(getThingsToDo(todos))
+sortTodos(todos)
+console.log(todos)
+
+// console.log(getThingsToDo(todos))
  
 // deleteTodo(todos, '!!buy food')
 // console.log(todos)
