032fd5d2095402c9dbb18c791ee89fcf8c67e88d
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-11 18:35:20 +0200

    queries

--- JavaScript/notes-app/notes-app.js
+++ JavaScript/notes-app/notes-app.js
@@ -1,14 +1,20 @@
-// DOM - Document Object Model
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
-// Query and remove
-// const p = document.querySelector('p')
-// p.remove()
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'
+})
 
-// Query all and remove
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function (p) {
-    p.textContent = '******'
-    // console.log(p.textContent)
-    // p.remove()
+document.querySelector('#remove-all').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function( note) {
+        note.remove()
+    })
 })
