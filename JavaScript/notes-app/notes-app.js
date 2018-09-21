a8ed0d999f31d257c678506b087d72498f59e1fe
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-09-21 19:31:06 +0200

    edition

--- JavaScript/notes-app/notes-app.js
+++ JavaScript/notes-app/notes-app.js
@@ -1,19 +1,48 @@
-const notes = getSavedNotes()
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Andrew',
+//     age: 27
+// }
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
-    notes.push({
-        id: uuidv4(),
-        title: '',
-        body: ''
-    })
-    saveNotes(notes)
-    renderNotes(notes, filters)
+    e.target.textContent = 'The button was clicked'
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
