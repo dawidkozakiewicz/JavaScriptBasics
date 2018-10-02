85b08e9b5744d3d1519fe4d003a5990016fa758f
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-10-02 22:15:45 +0200

    subclasses

--- JavaScript/oop/person.js
+++ JavaScript/oop/person.js
@@ -1,4 +1,5 @@
 // Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
 
 class Person {
     constructor(firstName, lastName, age, likes = []) {
@@ -23,9 +24,34 @@ class Person {
     }
 }
 
-const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
-me.setName('Alexis Turner')
-console.log(me.getBio())
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
+}
 
-const person2 = new Person('Clancey', 'Turner', 51)
-console.log(person2.getBio())
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+        this.grade += change
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
+}
+
+const me = new Student('Andrew', 'Mead', 27, 88, [])
+console.log(me.getBio())
+me.updateGrade(-20)
+console.log(me.getBio())
