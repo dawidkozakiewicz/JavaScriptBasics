3bac541c3d1e24cf963238bb3b855b6ad09eafa7
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-10-09 09:32:03 +0200

    promises and new files in 'metody'

--- JavaScript/functions/promises.js
+++ JavaScript/functions/promises.js
@@ -15,14 +15,15 @@ getDataCallback((err, data) => {
 })
 
 // Promise
-const myPromise = new Promise((resolve, reject) => {
+const getDataPromise = (data) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve('This is the promise data')
-        reject('This is my promise error')
-        reject('This is my promise error')
+        resolve(`This is my success data: ${data}`)
+        // reject('This is my promise error')
+        // reject('This is my promise error')
     }, 2000)
 })
 
+const myPromise = getDataPromise(123)
 myPromise.then((data) => {
     console.log(data)
 }, (err) => {
--- metody/promises.js
+++ metody/promises.js
@@ -1,19 +1,3 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-
-    request.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            resolve(data.puzzle)
-        } else if (e.target.readyState === 4) {
-            reject('An error has taken place')
-        }
-    })
-
-    request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    request.send()
-})
-
 const getCountry = (countryCode) => new Promise((resolve, reject) => {
     const countryRequest = new XMLHttpRequest()
 
@@ -29,4 +13,12 @@ const getCountry = (countryCode) => new Promise((resolve, reject) => {
 
     countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
     countryRequest.send()
+})
+
+
+
+getCountry('MX').then((country) => {
+    console.log(country.name)
+}, (err) => {
+    console.log(`Error: ${err}`)
 })
