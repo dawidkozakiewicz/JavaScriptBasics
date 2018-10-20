d75e336f283e63c06624f908dd8427055bf5c64d
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-10-20 20:05:07 +0200

    changes in hangman and todo

--- projekt_kaczuszka/notes-app/scripts/notes-functions.js
+++ projekt_kaczuszka/notes-app/scripts/notes-functions.js
@@ -26,10 +26,10 @@ const removeNote = (id) => {
 }
 
 // Generate the DOM structure for a note
-const generateNoteDOM = (note) => {
+const generateNoteDOM = (note) => {  // jest tu przekazana każada note po kolei 104.
     const noteEl = document.createElement('a') //tworzy miejsce na link
-    const textEl = document.createElement('p') // tworzy miejsce na paragraf
-    const statusEl = document.createElement('p') // tworzy miejsce na paragraf
+    const textEl = document.createElement('p') // tworzy miejsce na paragraf (będzie to tytuł notatki) 36.
+    const statusEl = document.createElement('p') // tworzy miejsce na paragraf (będzie to status notatki)
 
     // Setup the note title text
     if (note.title.length > 0) {
@@ -37,17 +37,17 @@ const generateNoteDOM = (note) => {
     } else {
         textEl.textContent = 'Unnamed note'
     }
-    textEl.classList.add('list-item__title')
-    noteEl.appendChild(textEl)
+    textEl.classList.add('list-item__title')  // nadaje CSS tytułowi notatki
+    noteEl.appendChild(textEl) //wrzuca tytuł notatki do linku
 
     // Setup the link
-    noteEl.setAttribute('href', `/edit.html#${note.id}`)
-    noteEl.classList.add('list-item')
+    noteEl.setAttribute('href', `/edit.html#${note.id}`)  //ustawia atrybyt href dla linku z tytulem notatki
+    noteEl.classList.add('list-item')  // nadaje CSS linkowi 'a'
 
     // Setup the status message
-    statusEl.textContent = generateLastEdited(note.updatedAt)
-    statusEl.classList.add('list-item__subtitle')
-    noteEl.appendChild(statusEl)
+    statusEl.textContent = generateLastEdited(note.updatedAt) // tekst z funkcji w 115.
+    statusEl.classList.add('list-item__subtitle')  //dodaje CSS do owej informacji
+    noteEl.appendChild(statusEl) // wrzuca paragraf do linku
 
     return noteEl
 }
