6360ba99aa7d82b6fcdaf75e24db31b2c39ce295
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-11-01 18:23:47 +0100

    adding spread

--- JavaScript/boilerplate/src/index.js
+++ JavaScript/boilerplate/src/index.js
@@ -1,23 +1,16 @@
-const calculateAverage = (thing, ...numbers) => {
-    //return (numOne + numTwo) / 2
-    let sum = 0
-    numbers.forEach((num) => sum += num)
-    const average = sum / numbers.length
-    return `The average ${thing} is ${average}`
-}
-
-console.log(calculateAverage('age', 0, 100, 88, 64))
-
-// Create printTeam that takes team name, coach, and players
-
-// Team: Liberty
-// Coach: Casey Penn
-// Players: Marge, Aiden, Herbert, Sherry
-
 const printTeam = (teamName, coach, ...players) => {
     console.log(`Team: ${teamName}`)
     console.log(`Coach: ${coach}`)
     console.log(`Players: ${players.join(', ')}`)
 }
 
-printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+const team = {
+    name: 'Libery',
+    coach: 'Casey Penn',
+    players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+}
+printTeam(team.name, team.coach, ...team.players)
+
+let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
+cities = [...cities, 'Santiago']
+console.log(cities)
