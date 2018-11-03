e5216b05aa0209e2d44ce6bafaa8810807e3fe27
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-11-03 23:14:40 +0100

    duck changes

--- JavaScript/boilerplate/src/index.js
+++ JavaScript/boilerplate/src/index.js
@@ -1,16 +1,24 @@
-const printTeam = (teamName, coach, ...players) => {
-    console.log(`Team: ${teamName}`)
-    console.log(`Coach: ${coach}`)
-    console.log(`Players: ${players.join(', ')}`)
+const todo = {
+    id: 'asdfpoijwermasdf',
+    text: 'Pay the bills',
+    completed: false,
+    car: 'Syrena 105'
 }
 
-const team = {
-    name: 'Libery',
-    coach: 'Casey Penn',
-    players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+const printTodo = ({ text, completed }) => {
+    console.log(`${text}: ${completed}`)
 }
-printTeam(team.name, team.coach, ...team.players)
+printTodo(todo)
 
-let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
-cities = [...cities, 'Santiago']
-console.log(cities)
+const { text:todoText, completed, details = 'No details provided', ...others } = todo
+
+console.log(todoText)
+console.log(completed)
+console.log(details)
+console.log(others)
+
+const age = [65, 0, 13]
+const [firstAge, ...otherAges] = age
+
+console.log(firstAge)
+console.log(otherAges)
