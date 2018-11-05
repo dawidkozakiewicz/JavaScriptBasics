10006643b995f956d664e438638310f34e067d0e
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-11-05 12:20:59 +0100

    changes in spread

--- metody/spread.js
+++ metody/spread.js
@@ -3,7 +3,7 @@
 // Poniższy kod pokazuje, że dane, do których masz dostęp, mają graczy przechowywanych w tablicy.
 // Problem polega na tym, że printTeam nie oczekuje argumentu tablicowego. 
 // Oczekuje graczy jako indywidualnych argumentów.
-// Używając składni rozłożonej, możesz wywołać printTeam z poprawnymi argumentami.
+// Używając składni spread, możesz wywołać printTeam z poprawnymi argumentami.
 
 const printTeam = (teamName, coach, ...players) => {
     console.log(`Team: ${teamName}`)
@@ -18,3 +18,16 @@ const team = {
 }
 
 printTeam(team.name, team.coach, ...team.players)
+
+
+// Składnia spread może być również używana podczas tworzenia tablicy. 
+// Przydaje się do dodawania elementów do tablicy lub kopiowania tablic. 
+// Poniższy kod wykorzystuje składnię spread do klonowania tablicy i dodawania do niej nowego elementu.
+
+
+let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
+let citiesClone = [...cities, 'Santiago']
+
+console.log(cities) // Will print three citites
+
+console.log(citiesClone) // Will print four citites
