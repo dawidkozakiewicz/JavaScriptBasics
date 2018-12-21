4067ad2b7ef44d748f371b34a787711a7e177dd3
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-12-21 20:41:44 +0100

    some christmas orders

--- metody/MetodyWMiareNowe/destructuring.js
+++ metody/MetodyWMiareNowe/destructuring.js
@@ -1,56 +1,45 @@
 // W JavaScript, często masz cały obiekt przekazany do funkcji, gdy funkcja potrzebuje tylko kilku wartości.
-// Destructuring pozwoli ci wyciągnąć własności z obiektów. 
-// Pozwoli ci też wyciągać przedmioty z tablic. 
-// Poniższy kod pokazuje, w jaki sposób można użyć destructuring, aby wyciągnąć właściwości z obiektu. 
-// Tworzy zmienne indywidualne dla właściwości obiektu, do których można uzyskać bezpośredni dostęp. 
+// Destructuring pozwoli ci wyciągnąć własności z obiektów.
+// Pozwoli ci też wyciągać przedmioty z tablic.
+// Poniższy kod pokazuje, w jaki sposób można użyć destructuring, aby wyciągnąć właściwości z obiektu.
+// Tworzy zmienne indywidualne dla właściwości obiektu, do których można uzyskać bezpośredni dostęp.
 // Poniższy kod wykorzystuje także parametr rest ...other utworzy nowy obiekt ze wszystkimi właściwościami, które nie zostały zdestrukturyzowane.
 
-
 const todo = {
-    id: 'asdfpoijwermasdf',
-    text: 'Pay the bills',
-    completed: false
-}
+	id: 'asdfpoijwermasdf',
+	text: 'Pay the bills',
+	completed: false
+};
 
-const {
-    text: todoText,
-    completed,
-    details = 'No details provided',
-    ...others
-} = todo
+const { text: todoText, completed, details = 'No details provided', ...others } = todo;
 
-console.log(todoText) // Will print: "Pay the bills"
-console.log(completed) // Will print: false
-console.log(details) // Will print: "No details provided"
-console.log(others) // Will print: { id: "asdfpoijwermasdf" }
+console.log(todoText); // Will print: "Pay the bills"
+console.log(completed); // Will print: false
+console.log(details); // Will print: "No details provided"
+console.log(others); // Will print: { id: "asdfpoijwermasdf" }
 
-// Destructuring działa również z argumentami funkcji. 
-// Poniższa funkcja printTodo oczekuje argumentu obiektu. Zamiast nazywania argumentu todo i uzyskiwania właściwości z niego, 
+// Destructuring działa również z argumentami funkcji.
+// Poniższa funkcja printTodo oczekuje argumentu obiektu. Zamiast nazywania argumentu todo i uzyskiwania właściwości z niego,
 // jest on destrukturyzowany w argumentach funkcji w celu zapewnienia dostępu do 'text' i 'completed' właściwości jako zmiennych.
 
 const todo = {
-    id: 'asdfpoijwermasdf',
-    text: 'Pay the bills',
-    completed: false
-}
+	id: 'asdfpoijwermasdf',
+	text: 'Pay the bills',
+	completed: false
+};
 
-const printTodo = ({
-    text,
-    completed
-}) => {
-    console.log(`${text}: ${completed}`)
-}
+const printTodo = ({ text, completed }) => {
+	console.log(`${text}: ${completed}`);
+};
 
-printTodo(todo)
+printTodo(todo);
 
-
-
-// Destrukturyzacja działa również z tablicami. 
-// Tablice mogą być destrukturyzowane samodzielnie lub jako argument funkcji. 
-// Poniższy kod demonstruje to przez pobranie pierwszej wartości tablicy i zapisanie jej w firstAge. 
+// Destrukturyzacja działa również z tablicami.
+// Tablice mogą być destrukturyzowane samodzielnie lub jako argument funkcji.
+// Poniższy kod demonstruje to przez pobranie pierwszej wartości tablicy i zapisanie jej w firstAge.
 // Następnie używa parametru rest, aby pobrać pozostałe wartości i zapisać je w otherAges.
 
-const age = [65, 0, 13]
-const [firstAge, ...otherAges] = age
-console.log(firstAge) // Will print: 65
-console.log(otherAges) // Will print: [0, 13]
+const age = [ 65, 0, 13 ];
+const [ firstAge, ...otherAges ] = age;
+console.log(firstAge); // Will print: 65
+console.log(otherAges); // Will print: [0, 13]
