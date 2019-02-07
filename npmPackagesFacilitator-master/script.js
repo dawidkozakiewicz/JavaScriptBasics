1c2e60c4feda9433422d2b4edfd58abb14f0d727
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-02-07 23:11:47 +0100

    better icons in cards

--- npmPackagesFacilitator-master/script.js
+++ npmPackagesFacilitator-master/script.js
@@ -1,13 +1,11 @@
 let packages = {
-    "babel-cli": "^6.26.0",
-    "babel-loader": "^7.1.4",
-    "babel-polyfill": "^6.26.0",
-    "babel-preset-env": "^1.6.1",
-    "live-server": "^1.2.0",
-    "uuid": "^3.2.1",
-    "webpack": "^4.5.0",
-    "webpack-cli": "^2.0.14",
-    "webpack-dev-server": "^3.1.3"
+    "axios": "^0.18.0",
+    "react": "^16.4.1",
+    "react-dom": "^16.4.1",
+    "react-redux": "^5.0.7",
+    "react-router-dom": "^4.3.1",
+    "react-scripts": "1.1.4",
+    "redux": "^4.0.0"
   }
 
   let tablica = Object.keys(packages)
